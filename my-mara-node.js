0afc3419600ef9840f5a26fdd39481ad7f15e00e
taher1499 <89098047+taher1499@users.noreply.github.com>
2022-04-14 01:43:36 -0700

    old changes

--- my-mara-node.js
+++ my-mara-node.js
@@ -554,10 +554,7 @@ class MyMaraNode {
                   if (objectids.includes(messageHash)) {
                     console.log("Object already present in database");
                   } else {
-                    if (
-                      this.validation(this._knownObjects, message.object) ===
-                      true
-                    ) {
+                    if (this.validation(this._knownObjects, message.object)) {
                       await this._knownObjects.put(messageHash, message.object);
 
                       const IHaveObject = {
@@ -567,12 +564,7 @@ class MyMaraNode {
 
                       socket.write(canonicalize(IHaveObject) + "\n");
                     } else {
-                      const error = {
-                        type: "error",
-                        error: "Unsupported message type received",
-                      };
                       console.log("message invalid");
-                      socket.write(canonicalize(error) + "\n");
                     }
                   }
                 } else if (message.type === "getobject" && handshake === true) {
